c564ad6764fe71137150eef88c3160b9cf6570a5
pandrey76 <pandrey76@yandex.ru>
2020-11-11 14:22:43 +0300

    Remove some mistakes.
    Added models to 'mailings' application and remove there from 'donation' application.

--- src/mailings/models.py
+++ src/mailings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Создаём две отдельные модели
+
+
+class CommonMailingList(models.Model):
+    """Рассылка на общие материалы с сайта"""
+    email = models.EmailField('Email подписчика')
+
+    class Meta:
+        # Имя таблички в базе данных
+        db_table = "common_mailing_list"
+
+
+class CaseMailingList(models.Model):
+    """Рассылка на материалы конкретного дела"""
+    email = models.EmailField('Email подписчика')
+    case = models.ForeignKey(to='cases.Case', verbose_name='Дело', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'case_mailing_list'
