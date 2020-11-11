cac470091d7705f8cf250f60eeb6a3aed4a9bd84
pandrey76 <pandrey76@yandex.ru>
2020-11-11 17:12:26 +0300

    Install mailchimp client.

--- src/mailings/views.py
+++ src/mailings/views.py
@@ -3,5 +3,10 @@ from django.http import JsonResponse
 
 def add_to_common_list_view(request):
     """Веб-сервис добавляющий email общий лист рассылки"""
+    email = request.GET.get('email')
+
+    if not email:
+        return JsonResponse({'success': False, 'message': 'Передайте email'})
+
     # Допустим у нас простинький сервис который возвращает только такой Json.
-    return JsonResponse({'success':True})
+    return JsonResponse({'success': True})
