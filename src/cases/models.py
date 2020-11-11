93c2e27a5f9a972592a8bfbd675cb3419f2b1ec1
pandrey76 <pandrey76@yandex.ru>
2020-11-11 13:43:45 +0300

    Created models in cases applications.

--- src/cases/models.py
+++ src/cases/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Моделька для хранения дела
+class Case(models.Model):
+    """Дело на сайте - набор материалов по конкретному делу"""
+    name = models.CharField('Название дела', max_length=255)
+
+    class Meta:
+        db_table = 'case'
