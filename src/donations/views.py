863a250412f9e32a6f500e15e409c9f573244f56
pandrey76 <pandrey76@yandex.ru>
2020-11-12 16:58:43 +0300

    Calling our business logic from Donates application.

--- src/donations/views.py
+++ src/donations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from mailings.mailchimp_services import add_mailchimp_email_with_tag
+
+# Вебхук, который пришёл о факте успешной оплаты из какой-нибудь платёжной системы
+# нащей системы Донатов.
+def webhook(request):
+    """Обработчик Вебхук от платёжной системы"""
+
+    # Здесь нам надо подтвердить, что наш платёж правильный, сохранить эту всю историю в базу данных и
+    # помимо всего прочего нам нуно отправить этот email в некую аудиторию mailchimp.
+    add_mailchimp_email_with_tag(email=request.POST.get('email'),
+                                 audience_name='DONATES',
+                                 tag='DONATE')
