2a82b82a408456bfe6a800adf038c61f53c7927b
pandrey76 <pandrey76@yandex.ru>
2020-11-12 09:58:29 +0300

    Make full view by adding client email to mailshimp list.
    Our view must work ok!!

--- src/mailings/views.py
+++ src/mailings/views.py
@@ -5,23 +5,39 @@ from django.conf import settings
 
 
 def add_to_common_list_view(request):
-    """Веб-сервис добавляющий email общий лист рассылки"""
+    """Веб-сервис добавляющий email в общий лист рассылки в mailchimp"""
+    # Получаем email
     email = request.GET.get('email')
 
+    # Проверяем, что он не пустой
     if not email:
         return JsonResponse({'success': False, 'message': 'Передайте email'})
 
+    # Создаём mailchimp клиент
     mailchimp_client = MailChimp(
-                    mc_api=settings.MAILCHIMP_API_KEY,
-                    mc_user=settings.MAILCHIMP_USERNAME)
+        mc_api=settings.MAILCHIMP_API_KEY,
+        mc_user=settings.MAILCHIMP_USERNAME)
     # Далее нам надо добавить наш email в аудиторию common (аудиторию общих рассылок) и
     # и навесить на него какой-нибудь общий тэг, допустим тотже Common (какой-то общий тэг)
 
-    # Данный запрос добавит наш email в аудиторию Common
+    # Данный запрос добавит наш email в аудиторию Common mailchimp
     # Первый параметр - это итдентификатор нашей аудитории
     mailchimp_client.lists.members.create(settings.MAILCHIMP_COMMON_LIST_ID, {
         'email_address': email,
         'status': 'subscribed',
     })
+    # Hash нашего клиента в mailchimp (Идентификатор нашего email в аудитории mailchimp)
+    subscriber_hash = mailchimp_client \
+        .search_members \
+        .get(query=email, fields='exact_matches.members.id') \
+        .get('exact_matches') \
+        .get('members')[0].get('id')
+
+    # Добавляем tag нашему клиенту (email в mailchimp)
+    mailchimp_client.lists.members.tags.update(
+        list_id=settings.MAILCHIMP_COMMON_LIST_ID,
+        subscriber_hash=subscriber_hash,
+        data={'tag': [{'name': 'COMMON TAG', 'status': 'active'}]})
+
     # Допустим у нас простинький сервис который возвращает только такой Json.
     return JsonResponse({'success': True})
