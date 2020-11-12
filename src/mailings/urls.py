16b9d96ec0225a0961e0f6fd005b3c6a86ca620f
pandrey76 <pandrey76@yandex.ru>
2020-11-12 12:25:56 +0300

    Add next view "add_to_case_list_view".

--- src/mailings/urls.py
+++ src/mailings/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('add_to_common_list', views.add_to_common_list_view),
+    path('add_to_case_list_view', views.add_to_case_list_view),
 ]
--- src/mailings/views.py
+++ src/mailings/views.py
@@ -3,7 +3,8 @@ from mailchimp3 import MailChimp
 from django.http import JsonResponse
 from django.conf import settings
 
-from .models import CommonMailingList
+from .models import CommonMailingList, CaseMailingList
+from cases.models import Case
 
 
 def add_to_common_list_view(request):
@@ -46,3 +47,54 @@ def add_to_common_list_view(request):
 
     # Допустим у нас простинький сервис который возвращает только такой Json.
     return JsonResponse({'success': True})
+
+
+def add_to_case_list_view(request):
+    """Веб-сервис, добавляющий email в лист рассылок по конкретному делу"""
+
+    email = request.GET.get('email')
+
+    if not email:
+        return JsonResponse({'success': False, 'message': 'Передайте email'})
+
+    case_id = request.GET.get('case_id')
+    # Проверяем, что он не пустой
+    if not case_id:
+        return JsonResponse({'success': False, 'message': 'Передайте case_id'})
+
+    # Создаём mailchimp клиент
+      mailchimp_client = MailChimp(
+        mc_api=settings.MAILCHIMP_API_KEY,
+        mc_user=settings.MAILCHIMP_USERNAME)
+    # Далее нам надо добавить наш email в аудиторию common (аудиторию общих рассылок) и
+    # и навесить на него какой-нибудь общий тэг, допустим тотже Common (какой-то общий тэг)
+
+    # Данный запрос добавит наш email в аудиторию Common mailchimp
+    # Первый параметр - это итдентификатор нашей аудитории
+    mailchimp_client.lists.members.create(settings.MAILCHIMP_CASE_LIST_ID, {
+        'email_address': email,
+        'status': 'subscribed',
+    })
+    # Hash нашего клиента в mailchimp (Идентификатор нашего email в аудитории mailchimp)
+    subscriber_hash = mailchimp_client \
+        .search_members \
+        .get(query=email, fields='exact_matches.members.id') \
+        .get('exact_matches') \
+        .get('members')[0].get('id')
+
+    # Мы получаем дело из базы данных
+    case = Case.objects.get(pk=case_id)
+    case_tag = f'Case {case.name}'
+
+    # Добавляем tag нашему клиенту (email в mailchimp)
+    mailchimp_client.lists.members.tags.update(
+        list_id=settings.MAILCHIMP_CASE_CASE_ID,
+        subscriber_hash=subscriber_hash,
+        data={'tag': [{'name': case_tag, 'status': 'active'}]})
+
+    # Добавляем email в DB
+    CaseMailingList.objects.get_or_create(email=email, case=case)
+
+    # Допустим у нас простинький сервис который возвращает только такой Json.
+    return JsonResponse({'success': True})
+
